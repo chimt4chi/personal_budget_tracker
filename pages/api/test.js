4343ad60dc3a93dc4442b832cdd7351fe496b667
rohit singh <rs9990271.edu@gmail.com>
2025-09-14 20:16:11 +0530

    ui changes

---
+++ pages/api/test.js
@@ -0,0 +1,12 @@
+// pages/api/test.js
+import { pool } from "@/lib/db";
+
+export default async function handler(req, res) {
+  try {
+    const [rows] = await pool.query("SELECT NOW() AS current_time");
+    res.status(200).json({ success: true, time: rows[0].current_time });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, error: err.message });
+  }
+}

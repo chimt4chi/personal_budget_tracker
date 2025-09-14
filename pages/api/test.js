c819d4917de1ce6dc6fb5fe547aa15cf61148e93
rohit singh <rs9990271.edu@gmail.com>
2025-09-14 21:37:23 +0530

    still connecting db

--- pages/api/test.js
+++ pages/api/test.js
@@ -3,7 +3,8 @@ import { pool } from "@/lib/db";
 
 export default async function handler(req, res) {
   try {
-    const [rows] = await pool.query("SELECT NOW() AS current_time");
+    const [rows] = await pool.query("SELECT NOW() AS now_time");
+
     res.status(200).json({ success: true, time: rows[0].current_time });
   } catch (err) {
     console.error(err);
